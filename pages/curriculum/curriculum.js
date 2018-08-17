5f7366eeba9e1cd14ebea12133c8cb82c611eacb
gansiqiu <1223720954@qq.com>
2018-08-17 17:38:29 +0800

    fix

--- pages/curriculum/curriculum.js
+++ pages/curriculum/curriculum.js
@@ -4,24 +4,35 @@ const app = getApp()
 
 Page({
    data: {
-      dataList: [],
+      dataList: [
+         { id: 1, name: "课程1" }, { id: 2, name: "课程2" }, { id: 3, name: "课程3" }, { id: 4, name: "课程4" },
+         { id: 5, name: "课程5" }, { id: 6, name: "课程6" }, { id: 7, name: "课程7" }, { id: 8, name: "课程8" },
+         { id: 5, name: "课程5" }, { id: 6, name: "课程6" }, { id: 7, name: "课程7" }, { id: 8, name: "课程8" }
+      ],
       aoData: { limit: 10, offset: 0 }
    },
    //事件处理函数
-   checkAssignment: function (event){
-      console.log(event.currentTarget.dataset.assignmentId);
+   checkAssignmentManage: function (event){
+      wx.navigateTo({
+         url: '/pages/assignment/assignmentManage/assignmentManage?curriculumId=' + event.currentTarget.dataset.curriculumId,
+      })
+   },
+   checkAssignmentSubmission:function(event){
+      wx.navigateTo({
+         url: '/pages/assignment/assignmentSubmission/assignmentSubmission?curriculumId=' + event.currentTarget.dataset.curriculumId
+      })
    },
    onLoad:function(){
-      var that = this;
-      wx.request({
-         url: 'http://localhost:8080/Zeus/curriculum/getCurriculumListByPage',
-         data:this.data.aoData,
-         success:function(res){
-            console.log(res);
-            that.setData({
-               dataList:res.data.object.list
-            })
-         }
-      })
+      // var that = this;
+      // wx.request({
+      //    url: 'http://localhost:8080/Zeus/curriculum/getCurriculumListByPage',
+      //    data:this.data.aoData,
+      //    success:function(res){
+      //       console.log(res);
+      //       that.setData({
+      //          dataList:res.data.object.list
+      //       })
+      //    }
+      // })
    }
 })
--- pages/curriculum/curriculumManage/curriculumManage.js
+++ pages/curriculum/curriculumManage/curriculumManage.js
@@ -5,7 +5,11 @@ Page({
     * 页面的初始数据
     */
    data: {
-      dataList: [],
+      dataList: [
+         { id: 1, name: "课程1" }, { id: 2, name: "课程2" }, { id: 3, name: "课程3" }, { id: 4, name: "课程4" }, 
+         { id: 5, name: "课程5" },{ id: 6, name: "课程6" }, { id: 7, name: "课程7" }, { id: 8, name: "课程8" }, 
+         { id: 9, name: "课程9" }, { id: 10, name: "课程10" }, { id: 11, name: "课程11" }, { id: 12, name: "课程12" }
+      ],
       aoData: { limit: 10, offset: 0 }
    },
    /*
@@ -30,15 +34,15 @@ Page({
     */
    onLoad: function(options) {
       var that = this;
-      wx.request({
-         url: 'http://localhost:8080/Zeus/curriculum/getCurriculumListByPage',
-         data: this.data.aoData,
-         success: function (res) {
-            console.log(res);
-            that.setData({
-               dataList: res.data.object.list
-            })
-         }
-      })
+      // wx.request({
+      //    url: 'http://localhost:8080/Zeus/curriculum/getCurriculumListByPage',
+      //    data: this.data.aoData,
+      //    success: function (res) {
+      //       console.log(res);
+      //       that.setData({
+      //          dataList: res.data.object.list
+      //       })
+      //    }
+      // })
    },
 })
